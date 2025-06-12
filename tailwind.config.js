4fdcb6546962d31644ae7de788d890ad0eb4d1fe
tosaha <tomshartman@gmail.com>
2025-06-12 11:11:46 -0400

    Upgrade to Tailwind CSS v4.1.10 - Updated deps, configs, CSS imports - Build tested successfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./app/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
-    darkMode: "media",
-    theme: {},
-    variants: {},
-    /* eslint-disable @typescript-eslint/no-require-imports */
-    plugins: [require("@tailwindcss/forms")]
-    /* eslint-enable @typescript-eslint/no-require-imports */
+    darkMode: "media"
 };
